0e708dc09d53747b54fa846bd0934032dd5b8505
ASWIN KUMAR N <168455921+Aswinkumar24022004@users.noreply.github.com>
2025-05-22 23:47:29 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,225 @@
+// Custom Script
+// Developed by: Samson.Onna
+// Theme by: Webthemez.com
+var customScripts = {
+    profile: function () {
+        // portfolio
+        if ($('.isotopeWrapper').length) {
+            var $container = $('.isotopeWrapper');
+            var $resize = $('.isotopeWrapper').attr('id');
+            // initialize isotope
+            $container.isotope({
+                itemSelector: '.isotopeItem',
+                resizable: false, // disable normal resizing
+                masonry: {
+                    columnWidth: $container.width() / $resize
+                }
+            });
+            $("a[href='#top']").click(function () {
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+            $('.navbar-inverse').on('click', 'li a', function () {
+                $('.navbar-inverse .in').addClass('collapse').removeClass('in').css('height', '1px');
+            });
+            // Smooth scroll for any link with the 'scroll-link' class
+            // This will specifically handle your 'View' buttons
+            $('a.scroll-link').on('click', function(event) {
+                var $anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top
+                }, 950, 'easeInOutExpo'); // Using 950ms for consistency with onePageNav
+                event.preventDefault();
+            });
+            $('#filter a').click(function () {
+                $('#filter a').removeClass('current');
+                $(this).addClass('current');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 1000,
+                        easing: 'easeOutQuart',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+            $(window).smartresize(function () {
+                $container.isotope({
+                    // update columnWidth to a percentage of container width
+                    masonry: {
+                        columnWidth: $container.width() / $resize
+                    }
+                });
+            });
+        }
+    },
+    fancybox: function () {
+        // fancybox
+        $(".fancybox").fancybox();
+    },
+    onePageNav: function () {
+
+        $('#mainNav').onePageNav({
+            currentClass: 'active',
+            changeHash: false,
+            scrollSpeed: 950,
+            scrollThreshold: 0.2,
+            filter: '',
+            easing: 'swing',
+            begin: function () {
+                //I get fired when the animation is starting
+            },
+            end: function () {
+                //I get fired when the animation is ending
+            },
+            scrollChange: function ($currentListItem) {
+                //I get fired when you enter a section and I pass the list item of the section
+            }
+        });
+    },
+    slider: function () {
+        $('#da-slider').cslider({
+            autoplay: true,
+            bgincrement: 0
+        });
+    },
+    owlSlider: function () {
+        var owl = $("#owl-demo");
+        owl.owlCarousel();
+        // Custom Navigation Events
+        $(".next").click(function () {
+            owl.trigger('owl.next');
+        })
+        $(".prev").click(function () {
+            owl.trigger('owl.prev');
+        })
+    },
+    bannerHeight: function () {
+        var bHeight = $(".banner-container").height();
+        $('#da-slider').height(bHeight);
+        $(window).resize(function () {
+            var bHeight = $(".banner-container").height();
+            $('#da-slider').height(bHeight);
+        });
+    },
+    init: function () {
+        customScripts.onePageNav();
+        customScripts.profile();
+        customScripts.fancybox();
+        customScripts.slider();
+        customScripts.owlSlider();
+        customScripts.bannerHeight();
+    }
+}
+$('document').ready(function () {
+    customScripts.init();
+});
+// Custom Script
+// Developed by: Samson.Onna
+// Theme by: Webthemez.com
+var customScripts = {
+    profile: function () {
+        // portfolio
+        if ($('.isotopeWrapper').length) {
+            var $container = $('.isotopeWrapper');
+            var $resize = $('.isotopeWrapper').attr('id');
+            // initialize isotope
+            $container.isotope({
+                itemSelector: '.isotopeItem',
+                resizable: false, // disable normal resizing
+                masonry: {
+                    columnWidth: $container.width() / $resize
+                }
+            });
+            $("a[href='#top']").click(function () {
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+            $('.navbar-inverse').on('click', 'li a', function () {
+                $('.navbar-inverse .in').addClass('collapse').removeClass('in').css('height', '1px');
+            });
+            // This is where you might have added it last time, but let's ensure it's also at the end of the ready function.
+            // $('#filter a').click(function () { ... });
+            // $(window).smartresize(function () { ... });
+        }
+    },
+    fancybox: function () {
+        // fancybox
+        $(".fancybox").fancybox();
+    },
+    onePageNav: function () {
+
+        $('#mainNav').onePageNav({
+            currentClass: 'active',
+            changeHash: false,
+            scrollSpeed: 950,
+            scrollThreshold: 0.2,
+            filter: '',
+            easing: 'swing',
+            begin: function () {
+                //I get fired when the animation is starting
+            },
+            end: function () {
+                //I get fired when the animation is ending
+            },
+            scrollChange: function ($currentListItem) {
+                //I get fired when you enter a section and I pass the list item of the section
+            }
+        });
+    },
+    slider: function () {
+        $('#da-slider').cslider({
+            autoplay: true,
+            bgincrement: 0
+        });
+    },
+    owlSlider: function () {
+        var owl = $("#owl-demo");
+        owl.owlCarousel();
+        // Custom Navigation Events
+        $(".next").click(function () {
+            owl.trigger('owl.next');
+        })
+        $(".prev").click(function () {
+            owl.trigger('owl.prev');
+        })
+    },
+    bannerHeight: function () {
+        var bHeight = $(".banner-container").height();
+        $('#da-slider').height(bHeight);
+        $(window).resize(function () {
+            var bHeight = $(".banner-container").height();
+            $('#da-slider').height(bHeight);
+        });
+    },
+    init: function () {
+        customScripts.onePageNav();
+        customScripts.profile();
+        customScripts.fancybox();
+        customScripts.slider();
+        customScripts.owlSlider();
+        customScripts.bannerHeight();
+    }
+}
+$('document').ready(function () {
+    customScripts.init();
+
+    // --- NEW CODE: Add this block here ---
+    $('a.scroll-link').on('click', function(event) {
+        event.preventDefault(); // Prevent default anchor jump
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeOutQuart'); // Changed easing to 'swing' as it's built-in jQuery
+    });
+    $('a.scroll-linkk').on('click', function(event) {
+        event.preventDefault(); // Prevent default anchor jump
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 2000, 'easeInOutExpo'); // Changed easing to 'swing' as it's built-in jQuery
+    });
+    // --- END NEW CODE ---
+});
